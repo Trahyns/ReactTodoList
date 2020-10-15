91357b22b9e142597d33da8e109f6ce3678397ff
Toni R <trahyns@gmail.com>
2020-10-14 21:55:43 -0400

    update file

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AddTodo from "./AddTodo"
+import TodoList from './TodoList'
+import Top from './Top'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="todo-box">
+    <div className="todo-innerBox">
+      <Top />
+      <TodoList />
+      <AddTodo />
     </div>
-  );
-}
+  </div>
+)
 
-export default App;
+export default App
