ec0ad3c5a39a7aa729334a3e3db9a1765f6e9c97
Toni R <trahyns@gmail.com>
2020-10-14 23:45:58 -0400

    insert nav bar

---
+++ todo-app/src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    
+        return (
+            <div>
+                <ul className="nav">
+                    <li className="nav-item slam-left"><a href="#">What Should I Do?</a></li>
+                    <li className="nav-item"><a href="#">Home</a></li>
+                    <li className="nav-item"><a href="#">Tasks</a></li>
+                    <li className="nav-item"><a className="contact" href="#">Contact</a></li>
+                </ul>
+            </div>
+
+        );
+
+}
+
+export default Navbar
--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -3,14 +3,18 @@ import AddTodo from "./AddTodo"
 import TodoList from './TodoList'
 import Top from './Top'
 import './index.css'
+import Navbar from "../Nav.js"
 
 const App = () => (
+  <div>
+  <Navbar />
   <div className="todo-box">
     <div className="todo-innerBox">
       <Top />
       <TodoList />
       <AddTodo />
     </div>
+  </div>
   </div>
 )
 
