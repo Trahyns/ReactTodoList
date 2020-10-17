df39c516ecf4b5599b2a96bc61d21bb375ace6c8
Toni R <trahyns@gmail.com>
2020-10-17 13:59:14 -0400

    add container element and center todo app

--- todo-app/src/components/TodoApp.js
+++ todo-app/src/components/TodoApp.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import { Container } from 'react-bootstrap'
 
 const TodoApp = () => (
+    <Container className="center">
     <div>
         <AddTodo />
         <VisibleTodoList />
         <Footer />
     </div>
+    </Container>
 )
 
 export default TodoApp
